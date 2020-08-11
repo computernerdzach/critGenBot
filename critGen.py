84029f1e02943b509d7f29467e721879677cea4e
computernerdzach <wizteczach@gmail.com>
2020-08-11 08:54:11 -0700

    added functionality to quit with a command, bot announces its entry and exit with quotes from Futuramas Bender

--- critGen.py
+++ critGen.py
@@ -9,6 +9,11 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 channel1ID = int(os.getenv('CHANNEL1'))
 channel2ID = int(os.getenv('CHANNEL2'))
 client = discord.Client()
+greetings = ['Bite my shiny metal ass!',
+             "I'm still alive, baby!",
+             "Kill all humans!",
+             "critGenBot does not condone the cool cool crime of robbery!"
+             ]
 
 
 @client.event
@@ -16,8 +21,8 @@ async def on_ready():
     print(f'{client.user} has connected to Discord!')
     channel1 = client.get_channel(channel1ID)
     channel2 = client.get_channel(channel2ID)
-    await channel1.send('Bite my shiny metal ass!')
-    await channel2.send('Bite my shiny metal ass!')
+    await channel1.send(random.choice(greetings))
+    await channel2.send(random.choice(greetings))
 
 
 @client.event
@@ -25,31 +30,31 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    hits = ["Ludicrous Maneuver. All allies with Passive Perception 12 get +2 on nextroll.",
-            "Late Timing. As an immediate action attack the same opponent atdisadvantage.",
-            "Wide Open. Target incapacitated 1 round. No 1's or 2's on damagedie.",
-            "Guarded Strike. +2AC for 1 round. No 1's or 2's on damagedie.",
-            "Savage Chop. Ignore damage resistance. No 1's or 2's on damagedie.",
-            "Retaliation. As an immediate action attack the same opponent. No1's or 2's on damagedie.",
-            "Ruthless Assault. Extra damagedie.",
-            "Defensive Strike. +4 AC for 1 round. Extra damagedie.",
-            "Traumatic Injury. Ignore damage resistance. Extra damagedie.",
-            "Victimized. Opponent provokes attacks of opportunity. Extra damagedie.",
-            "Calamitous Fall. DC 13 DEX save or prone. Extra damagedie.",
-            "Disoriented. DC 13 CON save or stunned for 1 round. Extra damagedie.",
-            "Dirt in Eye. DC 13 WIS save or blinded for 1 round. Extra damagedie.",
-            "Bleeder. 1d6 bleeding damage per round. Extra damagedie.",
-            "Rout. Attacker has melee advantage and the target has disadvantagefor 1round. Extra damagedie.",
-            "The Gods Have Spoken. Roll twice and take the betterresult.",
-            "Momentum. As an immediate action attack the same opponent. Extradamagedie.",
+    hits = ["Ludicrous Maneuver. All allies with Passive Perception 12 get +2 on next roll.",
+            "Late Timing. As an immediate action attack the same opponent at disadvantage.",
+            "Wide Open. Target incapacitated 1 round. No 1's or 2's on damage die.",
+            "Guarded Strike. +2AC for 1 round. No 1's or 2's on damage die.",
+            "Savage Chop. Ignore damage resistance. No 1's or 2's on damage die.",
+            "Retaliation. As an immediate action attack the same opponent. No1's or 2's on damage die.",
+            "Ruthless Assault. Extra damage die.",
+            "Defensive Strike. +4 AC for 1 round. Extra damage die.",
+            "Traumatic Injury. Ignore damage resistance. Extra damage die.",
+            "Victimized. Opponent provokes attacks of opportunity. Extra damage die.",
+            "Calamitous Fall. DC 13 DEX save or prone. Extra damage die.",
+            "Disoriented. DC 13 CON save or stunned for 1 round. Extra damage die.",
+            "Dirt in Eye. DC 13 WIS save or blinded for 1 round. Extra damage die.",
+            "Bleeder. 1d6 bleeding damage per round. Extra damage die.",
+            "Rout. Attacker has melee advantage and the target has disadvantage for 1round. Extra damage die.",
+            "The Gods Have Spoken. Roll twice and take the better result.",
+            "Momentum. As an immediate action attack the same opponent. Extra damagedie.",
             "Great Hit. Ignore damage resistance. DoubleDamage.",
             "Inspirational Display. All allies receive advantage on next roll. DoubleDamage.",
             "Vertigo. Drop all items in hand. Knocked back 1d3 squares. DC 15DEX save or prone. Double Damage.",
             "Perplexed Reaction. Target incapacitated 1 round,provokes attacks of opportunity. Double Damage.",
-            "...and Stay Down. Prone and stunned 1 round. DC 15 CON save or movehalved until rest. Double Damage.",
-            "Astute Counter. As an immediate action attack the same opponent withadvantage. DoubleDamage.",
+            "...and Stay Down. Prone and stunned 1 round. DC 15 CON save or move halved until rest. Double Damage.",
+            "Astute Counter. As an immediate action attack the same opponent with advantage. DoubleDamage.",
             "Impaled. 1d10 bleeding damage per round. Stunned 1 round. TripleDamage.",
-            "Conspiracy. The next hit landed on the foe is an automatic criticalhit. Blinded 1 round. Triple Damage.",
+            "Conspiracy. The next hit landed on the foe is an automatic critical hit. Blinded 1 round. Triple Damage.",
             "Demoralized. Prone. Stunned 2 rounds. TripleDamage.",
             "Knockout. DC 16 CON save or unconscious. Prone. Stunned 2 rounds. Triple Damage.",
             "Final Strike. DC 18 CON save or die. Prone 1d4 squares back. Stunned 3rounds. QuadrupleDamage.",
@@ -76,24 +81,28 @@ async def on_message(message):
               "Nothing unusual happens.",
               ]
 
-    whisperText = f'{client.user} whispered result to {message.author}'
+    whisper_text = f'{client.user} whispered result to {message.author}'
+    bye = "Cheese it!"
 
     if '!hit' in message.content.lower():
         response = random.choice(hits)
         if 'whisper' in message.content.lower():
             await message.author.send(response)
-            await message.channel.send(whisperText)
-            print(f'{message.author} whispered a roll ' + response)
+            await message.channel.send(whisper_text)
+            print(f'{message.author} whispered a roll\n    ' + response)
         else:
             await message.channel.send(response)
-            print(f'{message.author} made a roll ' + response)
+            print(f'{message.author} made a roll\n    ' + response)
     elif '!miss' in message.content.lower():
         response = random.choice(hits)
         if 'whisper' in message.content.lower():
             await message.author.send(response)
-            await message.channel.send(whisperText)
-            print(f'{message.author} whispered a roll ' + response)
+            await message.channel.send(whisper_text)
+            print(f'{message.author} whispered a roll\n    ' + response)
         else:
             await message.channel.send(response)
-            print(f'{message.author} made a roll ' + response)
+            print(f'{message.author} made a roll\n    ' + response)
+    elif "!goodbye" or "!bye" in message.content.lower():
+        await message.channel.send(bye)
+        await client.close()
 client.run(TOKEN)
